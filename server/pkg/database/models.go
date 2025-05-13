3d9fbbefbe395a14351620c562226dc4091724d4
acrd39 <acrd39@gmail.com>
2025-05-13 11:14:28 +0800

    用户登入脚手架搭建

---
+++ server/pkg/database/models.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"uniqueIndex;not null"` // 唯一索引
+	Password string `gorm:"not null"`             // 加密后的密码
+	Email    string `gorm:"uniqueIndex"`          // 可选字段
+	Avatar   string // 头像URL
+}
+
+// 加密密码（在创建用户前调用）
+func (u *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(bytes)
+	return nil
+}
+
+// 验证密码
+func (u *User) CheckPassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+}
